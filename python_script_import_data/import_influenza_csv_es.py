fad157ed538de9d7f3ec73eb4b55035568e43fbb
Jérôme <jerome.quintin59@orange.fr>
2020-04-25 23:29:35 +0200

    Influenza import mongo

--- python_script_import_data/import_influenza_csv_es.py
+++ python_script_import_data/import_influenza_csv_es.py
@@ -1,18 +1,27 @@
 # Import d'elasticsearch
-from elasticsearch import Elasticsearch, helpers
+#from elasticsearch import Elasticsearch, helpers
+
+# Import mongo
+from pymongo import MongoClient
 
 # Récupération de la connexion à Elasticsearch
-es = Elasticsearch(hosts="elastic:changeme@localhost:9200")
+#es = Elasticsearch(hosts="elastic:changeme@localhost:9200")
+
+# Connexion à Mongo
+client = MongoClient("mongodb://localhost:27017/")
 
 # Import de la bibliothèque csv
 import csv
 
+#Conversion string en date
+from datetime import datetime
+
 #Import pour récupérer les fichiers de la grippe
 import os.path
 
-
 # Déclaration d'une fonction qui renvoie un itérable pour le stockage sur elasticsearch
 def generate_data(nom_fichier):
+    
     # Ouverture du fichier csv
     with open(nom_fichier) as csvfile:
         # with ... as ... : Equivalent à :
@@ -52,10 +61,10 @@ def generate_data(nom_fichier):
                 type_B_ND = 0
             if not type_B_total :
                 type_B_total = 0
-            if not type_B_Victoria :
-                type_B_Victoria = 0
-            if not total_positives :
+            if not total_negatives :
                 total_negatives = 0
+            if not total_positives :
+                total_positives = 0
             
             # On précise le schéma de l'objet json (dictionnaire python) à envoyer avec l'api bulk:
             # il est de la forme : 
@@ -77,21 +86,23 @@ def generate_data(nom_fichier):
                     "datefs": datefs,
                     "collected": collected,
                     "processed":processed,
-                    "type_A_H1": type_A_H1,
-                    "type_A_H1N1": type_A_H1N1,
-                    "type_A_H3": type_A_H3,
-                    "type_A_H5": type_A_H5,
-                    "type_A_NS": type_A_NS,
-                    "type_A_total": type_A_total,
-                    "type_B_Yamata": type_B_Yamata,
-                    "type_B_Victoria": type_B_Victoria,
-                    "type_B_ND": type_B_ND,
-                    "type_B_total": type_B_total,
-                    "total_positives": total_positives,
-                    "total_negatives": total_negatives,
+                    "type_A_H1": int(type_A_H1),
+                    "type_A_H1N1": int(type_A_H1N1),
+                    "type_A_H3": int(type_A_H3),
+                    "type_A_H5": int(type_A_H5),
+                    "type_A_NS": int(type_A_NS),
+                    "type_A_total": int(type_A_total),
+                    "type_B_Yamata": int(type_B_Yamata),
+                    "type_B_Victoria": int(type_B_Victoria),
+                    "type_B_ND": int(type_B_ND),
+                    "type_B_total": int(type_B_total),
+                    "total_positives": int(total_positives),
+                    "total_negatives": int(total_negatives),
                     "ILI": ILI
                 }
             }
+        
+
 def listdirectory(path): 
     fichier=[] 
     for root, dirs, files in os.walk(path): 
@@ -99,9 +110,14 @@ def listdirectory(path):
             fichier.append(os.path.join(root, i)) 
     return fichier
 
-liste_fichier = listdirectory('D:\\Formation_Big_Data\\projet\data\\')
+liste_fichier = listdirectory('.\\data\\Influenza\\')
+
+mydb = client["epidemics"]
+mycol = mydb["influenza"]
 
 for nom_fichier in liste_fichier:
+    mycol.insert_many(generate_data(nom_fichier))
+
 # Envoie des données via l'api bulk sur elasticsearch
 #Liste pour récupérer tout les fichiers de la grippe
-    helpers.bulk(es, generate_data(nom_fichier))
+ #   helpers.bulk(es, generate_data(nom_fichier))
