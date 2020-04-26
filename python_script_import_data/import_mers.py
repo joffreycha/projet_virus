1f4f021dd4dbe8c1239e511e32766c17a4a31a7b
unknown <gwendoline.richaut@gmail.com>
2020-04-26 18:08:52 +0200

    mers

---
+++ python_script_import_data/import_mers.py
@@ -0,0 +1,54 @@
+# Import mongo
+from pymongo import MongoClient
+
+# Connexion à Mongo
+client = MongoClient("mongodb://localhost:27017/")
+
+# Import de la bibliothèque csv
+import csv
+
+#Conversion string en date
+from datetime import datetime
+
+#Import pour récupérer les fichiers de la grippe
+import os.path
+
+# Déclaration d'une fonction qui renvoie un itérable pour le stockage 
+def generate_data(nom_fichier):
+    
+    # Ouverture du fichier csv
+    with open(nom_fichier) as csvfile:
+        # with ... as ... : Equivalent à :
+        # try:
+        #     csvfile = open(r'C:\Users\Guillaume\Desktop\Covid_project\Data\sars_2003_complete_dataset_clean.csv')
+        # except:
+        #     raise FileNotFoundError
+        spamreader = csv.reader(csvfile, delimiter=',')
+        next(spamreader)
+        # TODO : créer le dictionnaire JSON pour une ligne du fichier
+        for row in spamreader:
+            # On récupère chaque ligne sous la forme d'une liste
+            year, week, region, new_cases = row
+            
+            # On précise le schéma de l'objet json (dictionnaire python) à envoyer avec l'api bulk:
+            # il est de la forme : 
+            # {
+            #   "index" : [nom_de_l_index],
+            #   "_source" : [objet_json]
+            # }
+            yield {
+                "_index": "mers",
+                "_type": "doc",
+                # Si json : juste "_source": json_object
+                "_source": {
+                    "year": year,
+                    "week": week,
+                    "region": region,
+                    "new_cases": int(new_cases)
+                }
+            }
+        
+mydb = client["epidemics"]
+mycol = mydb["mers"]
+
+mycol.insert_many(generate_data(r'..\data\MERS\mers_world_2012_2019_weekly_who.csv'))
