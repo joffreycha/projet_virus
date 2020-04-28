d09e0432e0b467115f4a984e4415f483dbf6ba5e
JejeQ59 <62019894+JejeQ59@users.noreply.github.com>
2020-04-28 22:11:16 +0200

    modification import mongo pour être lu par spring

--- python_script_import_data/import_influenza_csv_es.py
+++ python_script_import_data/import_influenza_csv_es.py
@@ -3,9 +3,7 @@
 
 # Import mongo
 from pymongo import MongoClient
-
-# Récupération de la connexion à Elasticsearch
-#es = Elasticsearch(hosts="elastic:changeme@localhost:9200")
+from bson.objectid import ObjectId
 
 # Connexion à Mongo
 client = MongoClient("mongodb://localhost:27017/")
@@ -25,9 +23,9 @@ def generate_data(nom_fichier):
     # Ouverture du fichier csv
     with open(nom_fichier) as csvfile:
         # with ... as ... : Equivalent à :
-        # try:
+        #try:
         #     csvfile = open(r'C:\Users\Guillaume\Desktop\Covid_project\Data\sars_2003_complete_dataset_clean.csv')
-        # except:
+        #except:
         #     raise FileNotFoundError
         spamreader = csv.reader(csvfile, delimiter=',')
         next(spamreader)
@@ -35,6 +33,7 @@ def generate_data(nom_fichier):
         for row in spamreader:
             # On récupère chaque ligne sous la forme d'une liste
             country, who_region, region, year, week, dateds, datefs, collected, processed, type_A_H1, type_A_H1N1, type_A_H3, type_A_H5, type_A_NS, type_A_total, type_B_Yamata, type_B_Victoria, type_B_ND, type_B_total, total_positives, total_negatives, ILI = row
+
             if not collected :
                 collected = 0
             if not processed :
@@ -66,6 +65,8 @@ def generate_data(nom_fichier):
             if not total_positives :
                 total_positives = 0
             
+            objI = ObjectId()
+
             # On précise le schéma de l'objet json (dictionnaire python) à envoyer avec l'api bulk:
             # il est de la forme : 
             # {
@@ -73,10 +74,6 @@ def generate_data(nom_fichier):
             #   "_source" : [objet_json]
             # }
             yield {
-                "_index": "influenza",
-                "_type": "doc",
-                # Si json : juste "_source": json_object
-                "_source": {
                     "country": country,
                     "who_region": who_region,
                     "region": region,
@@ -99,7 +96,7 @@ def generate_data(nom_fichier):
                     "total_positives": int(total_positives),
                     "total_negatives": int(total_negatives),
                     "ILI": ILI
-                }
+                
             }
         
 
@@ -110,13 +107,13 @@ def listdirectory(path):
             fichier.append(os.path.join(root, i)) 
     return fichier
 
-liste_fichier = listdirectory(r'..\data\Influenza')
+liste_fichier = listdirectory(r'.\\data\\Influenza')
 
 mydb = client["epidemics"]
 mycol = mydb["influenza"]
 
 for nom_fichier in liste_fichier:
-    mycol.insert_many(generate_data(nom_fichier))
+    result = mycol.insert_many(generate_data(nom_fichier), True, True)  
 
 # Envoie des données via l'api bulk sur elasticsearch
 #Liste pour récupérer tout les fichiers de la grippe
